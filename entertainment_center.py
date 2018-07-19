82a225a94fe027be67f5aa7171392a8e210a0fdc
Kedar Naik <kedarnaik515@gmail.com>
2018-07-19 16:30:25 +0530

    Comments added.

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,8 @@
 import media
+
 import fresh_tomatoes
 
+#Creating instances of class Movie for different movies.
 iron_man = media.Movie("Iron Man",
                         "American superhero film on Genius, billionaire, playboy and philanthropist Tony Stark.",
                         "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
@@ -16,5 +18,8 @@ avatar = media.Movie("Avatar",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://youtu.be/5PSNL1qE6VY")
 
+#Creating a list of movies and store it in variable movies. 
 movies = [iron_man, titanic, avatar]
+
+#Pass the list of movies to the class open_movies_page of fresh_tomatoes html page.
 fresh_tomatoes.open_movies_page(movies)

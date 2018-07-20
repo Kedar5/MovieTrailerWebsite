2ff34e1f315ce45f6f21512de641e21b5c22b753
Kedar Naik <32413641+Kedar5@users.noreply.github.com>
2018-07-20 19:22:21 +0530

    Edited the file according to PEP-8

--- entertainment_center.py
+++ entertainment_center.py
@@ -4,26 +4,27 @@ import fresh_tomatoes
 
 # Creating instances of class Movie for different movies.
 iron_man = media.Movie("Iron Man",
-                        "American superhero film on Genius, billionaire, playboy and philanthropist Tony Stark.",
-                        "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
-                        "https://www.youtube.com/watch?v=8hYlB38asDY")
+                       "American superhero film on Tony Stark",
+                       "https://bit.ly/2rLG8b8",
+                       "https://www.youtube.com/watch?v=8hYlB38asDY")
 
 titanic = media.Movie("Titanic",
-                     "A fictionalized epic romance-disaster film of the sinking of the RMS Titanic.",
-                     "https://upload.wikimedia.org/wikipedia/en/2/22/Titanic_poster.jpg",
-                     "https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
+                      "A film on sinking of the RMS Titanic.",
+                      "https://bit.ly/2uKv6RE",
+                      "https://www.youtube.com/watch?v=2e-eXJ6HgkQ")
 
 avatar = media.Movie("Avatar",
                      "A marine on an alien planet",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://bit.ly/1JofiD1",
                      "https://youtu.be/5PSNL1qE6VY")
 
-# Creating a list of movies and store it in variable movies. 
+# Creating a list of movies and store it in variable movies.
 movies = [
     iron_man,
     titanic,
     avatar
 ]
 
-# Pass the list of movies as arguement to the class open_movies_page of fresh_tomatoes html page.
+""" Pass the list of movies as arguement to the
+class open_movies_page of fresh_tomatoes html page."""
 fresh_tomatoes.open_movies_page(movies)

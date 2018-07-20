b8cd7b69aaa9b6a0e0c09d2477ffc02031869d90
Kedar Naik <32413641+Kedar5@users.noreply.github.com>
2018-07-20 22:44:08 +0530

    Edited the file according to PEP-8.

--- media.py
+++ media.py
@@ -2,14 +2,25 @@ import webbrowser
 
 
 class Movie():
-    """Define the constructor for class Movie and
-    assign the instance variables required."""
+    """
+        The class Movie is used to store all the information related to movies.
+
+        Args:
+            movie_title (str): Movie title of different movies.
+            movie_storyline (str): Description of movie in few lines.
+            poster_image (str): image of the poster to be displayed.
+            trailer_youtube (str): YouTube trailer shortened url.
+    """
     def __init__(self,
                  movie_title,
                  movie_storyline,
                  poster_image,
                  trailer_youtube):
+        # Store movie title in instance variable title.
         self.title = movie_title
+        # Store story line in instance variable storyline.
         self.storyline = movie_storyline
+        # Store poster image in instance variable poster_image_url.
         self.poster_image_url = poster_image
+        # Store youtube url in instance variable trailer_youtube.
         self.trailer_youtube_url = trailer_youtube
